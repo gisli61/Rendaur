cc0f6d4b53016ed24f0dfdb738837bd5397b2a1c
Gísli Másson <gisli.masson@mac.com>
2019-05-29 22:47:59 +0000

    Initial Commit

---
+++ PluginViewer/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  PluginViewer
+//
+//  Created by Gísli Másson on 29/05/2019.
+//  Copyright © 2019 Gísli Másson. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ PluginViewer/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  PluginViewer
+//
+//  Created by Gísli Másson on 29/05/2019.
+//  Copyright © 2019 Gísli Másson. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
