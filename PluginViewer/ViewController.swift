22dba73c0b36076f4c03873031e1f7e6cf24f9a3
Gísli Másson <gisli.masson@mac.com>
2019-06-08 23:33:59 +0000

    More code cleanup. Code still working

--- PluginViewer/ViewController.swift
+++ PluginViewer/ViewController.swift
@@ -16,15 +16,15 @@ class ViewController: NSViewController {
     var currentInstrument:AUAudioUnit?
     var currentAVInstrument:AVAudioUnit?
     private var midiFilePlayer:MidiFilePlayer?
-    private var myWindowController: NSWindowController? // Temporary store
     private var testWindowController: NSWindowController?
 
     //MARK: Properties
     @IBOutlet weak var pluginPopup: NSPopUpButton!
-    @IBOutlet weak var openPluginButton: NSButton!
+    //@IBOutlet weak var openPluginButton: NSButton!
     @IBOutlet weak var playMidiButton: NSButton!
     @IBOutlet weak var stopButton: NSButton!
     
+    //MARK: Methods
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -44,54 +44,6 @@ class ViewController: NSViewController {
         }
     }
     
-    private func createAUWindow(viewController: NSViewController,
-                                audioUnit: AVAudioUnit,
-                                identifier: Int,
-                                origin: NSPoint? = nil,
-                                color: NSColor? = nil) {
-        let incomingFrame = viewController.view.frame
-        let windowController = AudioUnitWindow(audioUnit: audioUnit)
-        
-        guard let unitWindow = windowController.window else {
-            print("Have no window")
-            return
-        }
-        guard let auName = audioUnit.auAudioUnit.audioUnitName else {
-            print("Have no audioUnitName")
-            return
-        }
-        
-        let winId = windowPrefix + String(identifier)
-        //let windowFrame = NSRect(origin: origin,
-        //                         size: NSSize(width: incomingFrame.width, height: incomingFrame.height + 30))
-        unitWindow.delegate = self
-        
-        windowController.scrollView?.removeFromSuperview()
-        windowController.contentViewController = viewController
-        
-        view.window?.addChildWindow(unitWindow, ordered: NSWindow.OrderingMode.above)
-    }
-    
-    public func showAudioUnit(_ audioUnit: AVAudioUnit) {
-        audioUnit.auAudioUnit.requestViewController { [weak self] viewController in
-            guard let strongSelf = self else {
-                print("self missing")
-                return
-            }
-            
-            guard let ui = viewController else {
-                print("no viewController!")
-                return
-            }
-            
-            DispatchQueue.main.async {
-                strongSelf.createAUWindow(viewController:ui,
-                                          audioUnit: audioUnit,
-                                          identifier: 6)
-            }
-        }
-        
-    }
     //MARK: Actions
     @IBAction func changePlugin(_ sender: NSPopUpButton) {
         guard let pluginName = sender.titleOfSelectedItem else {
@@ -110,80 +62,6 @@ class ViewController: NSViewController {
             print("No plugin selected!")
             return
         }
-        
-        /*
-        current.requestViewController() { [weak self] viewController in
-            guard let strongSelf = self else {
-                print("Self is nil")
-                return
-            }
-            
-            guard let vc = viewController else {
-                print("viewController is nil")
-                return
-            }
-            print("have a view controller")
-            DispatchQueue.main.async {
-                strongSelf.myWindowController = MyWindowController()
-                //strongSelf.myWindowController = AudioUnitWindow()
-                
-                guard let unitWindow = strongSelf.myWindowController?.window else {
-                    print("got no window")
-                    return
-                }
-                unitWindow.title = "MyAudioUnit"
-                //unitWindow.delegate = strongSelf
-                
-                strongSelf.myWindowController!.contentViewController = vc
-                strongSelf.myWindowController!.showWindow(nil)
-
-            }
-        }
-       */
-
-    }
-
-    @IBAction func openTestWindow(_ sender:NSButton) {
-
-        testWindowController = TestWindow()
-        guard let wc = testWindowController else {
-            print("Could not get window")
-            return
-        }
-        print(wc.windowNibName ?? "No window nib name")
-        wc.showWindow(nil)
-        if wc.window == nil {
-            print("No window here either")
-        }
-        
-        
-    }
-    
-    @IBAction func openPluginWindow(_ sender:NSButton) {
-        guard let currentAV = currentAVInstrument else {
-            print("No plugin selected!")
-            return
-        }
-        guard let current = currentInstrument else {
-            print("No plugin selected!")
-            return
-        }
-        
-        //showAudioUnit(currentAV)
-        
-        current.requestViewController() { nsViewController in
-            //guard let vc = nsViewController else {
-            //    print("viewController is nil")
-            //    return
-            //}
-            print("have a view controller")
-            let wc = MyWindowController()
-            //wc.showWindow(nil)
-            self.myWindowController = wc
-            self.myWindowController!.showWindow(nil)
-            print("Got here")
-        }
-        
 
     }
     
@@ -241,9 +119,7 @@ class ViewController: NSViewController {
         
         auWindowController.showWindow(nil)
     }
-    
-    
-    
+
 }
 
 extension ViewController: NSWindowDelegate {
